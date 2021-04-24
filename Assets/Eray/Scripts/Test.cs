0fbf175825ea815c0f50261d8f8977fef4339d1a
Eray Baran İnan <eraybaraninan@gmail.com>
2021-04-24 12:30:44 +0300

    namespace ile düzelttim classı

--- Assets/Eray/Scripts/Test.cs
+++ Assets/Eray/Scripts/Test.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+namespace Eray.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Test : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
