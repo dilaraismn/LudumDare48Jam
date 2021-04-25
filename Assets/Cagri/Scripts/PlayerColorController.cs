92f3f2e7dfc3064301396553c4836a3c9e6ef7ae
thrcgr <71606835+thrcgr@users.noreply.github.com>
2021-04-25 03:56:34 +0300

    fix: Color Manager

--- Assets/Cagri/Scripts/PlayerColorController.cs
+++ Assets/Cagri/Scripts/PlayerColorController.cs
@@ -17,28 +17,36 @@ namespace Cagri.Scripts
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                
+                ColorControl(KeyCode.Alpha1);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                
+                ColorControl(KeyCode.Alpha2);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                
+                ColorControl(KeyCode.Alpha3);
             } 
             else if (Input.GetKeyDown(KeyCode.Alpha4))
             {
-                
+                ColorControl(KeyCode.Alpha4);
             }
             else if (Input.GetKeyDown(KeyCode.Alpha5))
             {
-                
+                ColorControl(KeyCode.Alpha5);
             }
         }
 
-        private void ColorControl()
+        private void ColorControl(KeyCode targetKey)
         {
+            foreach (PlayerColor currentPlayerColor in GameManager.manager.currentPlayerColors)
+            {
+                if (currentPlayerColor.myKeyCode==targetKey)
+                {
+                    currentPlayerColor.isActive = !currentPlayerColor.isActive;
+                }
+            }
+            ColorManager.manager.OnPlayerChangeColor();
         }
 
     }
